86e304d44f9a2c1c84e6df2350307745bf9d7981
Timothy Ellersiek <NeilMcCauley@NeilMcCauley.fritz.box>
2016-09-07 20:51:41 +0200

    testing

--- app/components/ors-panel-routing/ors-options/ors-options.js
+++ app/components/ors-panel-routing/ors-options/ors-options.js
@@ -6,7 +6,6 @@ angular.module('orsApp.ors-options', []).component('orsOptions', {
     controller(orsSettingsFactory, orsObjectsFactory, orsUtilsService, orsRequestService, orsErrorhandlerService, orsParamsService) {
         var ctrl = this;
         ctrl.$onInit = function() {
-            console.log(ctrl.activeMenu)
         };
         ctrl.getClass = (bool) => {
             if (bool === true) return "fa fa-lg fa-fw fa-caret-up";
