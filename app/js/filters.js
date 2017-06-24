a1d4dff5194cf8fa01a1314e668164203b07e013
TimMcCauley <merusame@gmail.com>
2017-06-24 10:52:07 +0200

    rollover duration fixed, closes #110

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,11 +1,27 @@
 angular.module('orsApp.ors-filters', [])
     .filter('duration', () => {
         return (input) => {
-            let duration = new Date(input * 1000)
-                .toISOString()
-                .substr(11, 5);
-            if (duration == '00:00') return '<b>' + '< 1' + '</b>' + ' min';
-            else return '<b>' + duration + '</b>';
+            const days = Math.floor(input / 86400);
+            const hours = Math.floor(input / 3600);
+            input %= 3600;
+            minutes = Math.floor(input / 60);
+            seconds = input % 60;
+            if (days < 1 && hours < 1 && minutes < 1) {
+                return '<b>' + '< 1' + '</b>' + ' min';
+            } else {
+                let dayStr = '';
+                if (days > 0) {
+                    dayStr = days + 'd' + ' ';
+                }
+                let HHMM = [];
+                if (hours.toString()
+                    .length == 1) HHMM.push('0' + hours);
+                else HHMM.push(hours);
+                if (minutes.toString()
+                    .length == 1) HHMM.push('0' + minutes);
+                else HHMM.push(minutes);
+                return '<b>' + dayStr + HHMM.join(':') + '</b>';
+            }
         };
     })
     .filter('distance', ['orsSettingsFactory', (orsSettingsFactory) => {
