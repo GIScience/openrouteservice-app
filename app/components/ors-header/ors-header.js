09bef822424709e73184ac683e4f530faa0af076
TimMcCauley <merusame@gmail.com>
2017-09-04 22:16:36 +0200

    added tooltip and translations for #141

--- app/components/ors-header/ors-header.js
+++ app/components/ors-header/ors-header.js
@@ -3,23 +3,27 @@ angular.module('orsApp.ors-header', [])
         templateUrl: 'components/ors-header/ors-header.html',
         controller: ['$rootScope', '$timeout', '$translate', 'orsUtilsService', 'orsSettingsFactory', 'orsCookiesFactory', 'lists', function($rootScope, $timeout, $translate, orsUtilsService, orsSettingsFactory, orsCookiesFactory, lists) {
             let ctrl = this;
+            ctrl.changeExtras = () => {
+                orsUtilsService.setExtraInformation(ctrl.extra_infos);
+            };
             ctrl.$onInit = () => {
-                ctrl.extra_infos = {};
+                ctrl.extra_infos = {
+                    steepness: true
+                };
                 ctrl.lists_extra_info = lists.extra_info;
                 ctrl.getActiveProfile = orsSettingsFactory.getActiveProfile;
                 ctrl.optionList = lists.userOptions;
+                ctrl.changeExtras();
             };
             /** subscription to settings, when permalink is used with lang params
             we have to update language settings. This is called before panel settings
             object is defined, this is why we have two subscriptions */
             orsSettingsFactory.userOptionsSubject.subscribe(settings => {
                 ctrl.currentOptions = settings;
-                console.log(ctrl.currentOptions)
                 $translate.use(ctrl.currentOptions.language);
             });
             ctrl.changeOptions = (langChange) => {
                 if (langChange) $translate.use(ctrl.currentOptions.language);
-                console.log(ctrl.currentOptions);
                 orsSettingsFactory.setUserOptions(ctrl.currentOptions);
                 orsCookiesFactory.setCookieUserOptions(ctrl.currentOptions);
                 orsUtilsService.parseSettingsToPermalink(orsSettingsFactory.getSettings(), orsSettingsFactory.getUserOptions());
@@ -28,9 +32,6 @@ angular.module('orsApp.ors-header', [])
                 $timeout(() => {
                     $rootScope.$broadcast('rzSliderForceRender');
                 });
-            };
-            ctrl.changeExtras = () => {
-                orsUtilsService.setExtraInformation(ctrl.extra_infos);
             };
         }],
         bindings: {}
