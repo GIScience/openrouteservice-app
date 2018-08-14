c9393f5f6e355a7cacd3c5d1176e19caca0b9ae8
Amandus <amandusbutzer@gmx.de>
2018-08-14 15:36:16 +0200

    Fix cookie loading & refactor developer settings
    refactored the endpoint settings code so it won't interfere with the other cookie loading.
    API key can now be specified for API preset.

--- app/js/app.js
+++ app/js/app.js
@@ -41,7 +41,7 @@
             $httpProvider.interceptors.push(function($q, $document, $injector, lists, orsNamespaces, ENV, orsApikeyFactory, weathercheck) {
                 return {
                     'request': function(config) {
-                        const apiKey = orsApikeyFactory.getApiKey() === undefined ? weathercheck : orsApikeyFactory.getApiKey();
+                        const apiKey = ENV.key !== undefined ? ENV.key : orsApikeyFactory.getApiKey() === undefined ? weathercheck : orsApikeyFactory.getApiKey();
                         let ak = '?api_key=' + apiKey;
                         if (config.url === ENV.geocode + '/search' || config.url === ENV.geocode + '/reverse' || config.url === ENV.directions || config.url === ENV.isochrones || config.url === ENV.pois || config.url === ENV.matrix) {
                             config.url = config.url + ak;
