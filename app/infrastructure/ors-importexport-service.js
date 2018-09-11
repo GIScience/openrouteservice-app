36f62fb81d2cc44cd903eacf307c97b3429d0a3c
Amandus <amandusbutzer@gmx.de>
2018-09-11 13:56:37 +0200

    Fix gpx creation for empty description

--- app/infrastructure/ors-importexport-service.js
+++ app/infrastructure/ors-importexport-service.js
@@ -1,8 +1,7 @@
-angular
-  .module("orsApp.GeoFileHandler-service", ["ngFileSaver"])
-  /**
+angular.module("orsApp.GeoFileHandler-service", ["ngFileSaver"])
+/**
  /*** Export Factory
- **/ .factory("orsExportFactory", [
+ **/.factory("orsExportFactory", [
     "FileSaver",
     "Blob",
     "orsNamespaces",
@@ -10,193 +9,177 @@ angular
     "orsSettingsFactory",
     "orsUtilsService",
     "orsRouteService",
-    (
-      FileSaver,
-      Blob,
-      orsNamespaces,
-      orsRequestService,
-      orsSettingsFactory,
-      orsUtilsService,
-      orsRouteService
-    ) => {
-      var orsExportFactory = {};
-      /**
-       * Export any vector element on the map to GPX
-       * @param {Object} geometry: the route object (leaflet object) to export
-       * @param {String} geomType: the type of geometry which is passed
-       * @param {Object} options: export options
-       * @param {String} format: the file format to export
-       * @param {String} filename: the filename of the exported file
-       */
-      orsExportFactory.exportFile = (
-        geometry,
-        geomType,
-        options,
-        format,
-        filename
-      ) => {
-        let exportData, geojsonData, extension;
-        extension = "." + format;
-        switch (format) {
-            /**
-             * To generate the gpx file the ORS API is called with format=gpx and current settings.
-             * Instructions can be included when needed.
-             */
-          case "gpx":
-            let userOptions = orsSettingsFactory.getUserOptions();
-            let settings = orsSettingsFactory.getSettings();
-            let payload = orsUtilsService.routingPayload(settings, userOptions);
-            payload.format = "gpx";
-            if (!options.instructions) payload.instructions = false;
-            const request = orsRouteService.fetchRoute(payload);
-            orsRouteService.routingRequests.requests.push(request);
-            request.promise.then(
-              function(response) {
-                // parsing xml to js object using https://www.npmjs.com/package/xml-js
-                exportData = xml2js(response);
-                let metadata = exportData.elements[0].elements[0].elements;
-                metadata[0].elements[0].text = filename;
-                metadata[1].elements[0].text = options.instruction
-                  ? "This route and instructions were generated from maps.openrouteservice"
-                  : "This route was generated from maps.openrouteservice";
-                // parsing back to xml string + saving
-                exportData = js2xml(exportData);
+    (FileSaver, Blob, orsNamespaces, orsRequestService, orsSettingsFactory, orsUtilsService, orsRouteService) => {
+        var orsExportFactory = {}
+        /**
+         * Export any vector element on the map to GPX
+         * @param {Object} geometry: the route object (leaflet object) to export
+         * @param {String} geomType: the type of geometry which is passed
+         * @param {Object} options: export options
+         * @param {String} format: the file format to export
+         * @param {String} filename: the filename of the exported file
+         */
+        orsExportFactory.exportFile = (geometry, geomType, options, format, filename) => {
+            let exportData, geojsonData, extension
+            extension = "." + format
+            switch (format) {
+                /**
+                 * To generate the gpx file the ORS API is called with format=gpx and current settings.
+                 * Instructions can be included when needed.
+                 */
+                case "gpx":
+                    let userOptions = orsSettingsFactory.getUserOptions()
+                    let settings = orsSettingsFactory.getSettings()
+                    let payload = orsUtilsService.routingPayload(settings, userOptions)
+                    payload.format = "gpx"
+                    if (!options.instructions) payload.instructions = false
+                    const request = orsRouteService.fetchRoute(payload)
+                    orsRouteService.routingRequests.requests.push(request)
+                    request.promise.then(function (response) {
+                        // parsing xml to js object using https://www.npmjs.com/package/xml-js
+                        exportData = xml2js(response)
+                        let metadata = exportData.elements[0].elements[0].elements
+                        metadata[0].elements[0].text = filename
+                        // description node can be <desc></desc> so the parsed object wont have an elements array
+                        if (metadata[1].elements == null) {
+                            metadata[1].elements = [
+                                {text: "", type: "text"}
+                            ]
+                        }
+                        metadata[1].elements[0].text = options.instruction
+                            ? "This route and instructions were generated from maps.openrouteservice"
+                            : "This route was generated from maps.openrouteservice"
+                        // parsing back to xml string + saving
+                        exportData = js2xml(exportData)
+                        exportData = new Blob([exportData], {
+                            type: "application/xml;charset=utf-8"
+                        })
+                        FileSaver.saveAs(exportData, filename + extension)
+                    }, function (error) {
+                        orsSettingsFactory.requestSubject.onNext(false)
+                        alert("There was an error generating the gpx file: " + error)
+                    })
+                    // geojsonData = L.polyline(geometry).toGeoJSON();
+                    // geojsonData.properties.name = filename; // togpx is looking for name tag https://www.npmjs.com/package/togpx#gpx
+                    // exportData = togpx(geojsonData, {
+                    //   creator: "OpenRouteService.org"
+                    // });
+                    break
+                case "kml":
+                    geojsonData = L.polyline(geometry).toGeoJSON()
+                    exportData = tokml(geojsonData)
+                    break
+                case "rawjson":
+                    // removing nodes from the geometry data that is for sure not needed
+                    // by 3'rd party...
+                    delete geometry.extras
+                    delete geometry.geometryRaw
+                    delete geometry.$$hashKey
+
+                    // MARQ24: point_information have an massive effect on the actual file size!
+                    // So I am not 100% sure if this should be included or not in the exported
+                    // json data - personally I do not have any need for this info on my mobile
+                    // client - that's why IMHO it can/should be removed from the raw output
+                    delete geometry.point_information
+
+                    exportData = JSON.stringify(geometry)
+                    extension = ".json"
+                    break
+                case "geojson":
+                    if (geomType == "linestring") {
+                        if (!options.elevation) {
+                            angular.forEach(geometry, function (value) {
+                                value = value.splice(2, 1)
+                            })
+                        }
+                        exportData = JSON.stringify(L.polyline(geometry).toGeoJSON())
+                    } else if (geomType == "polygon") {
+                        let isochrones = []
+                        for (let i = 0; i < geometry.length; i++) {
+                            let properties = geometry[i].properties
+                            properties.id = i
+                            let c = geometry[i].geometry.coordinates
+                            for (let k = 0; k < c[0].length; k++) {
+                                let store = c[0][k][0]
+                                c[0][k][0] = c[0][k][1]
+                                c[0][k][1] = store
+                            }
+                            const geojsonpolygon = {
+                                type: "Feature", properties: properties, geometry: {
+                                    type: "Polygon", coordinates: c
+                                }
+                            }
+                            isochrones.push(geojsonpolygon)
+                        }
+                        exportData = JSON.stringify(isochrones)
+                        exportData = "{\"type\":\"FeatureCollection\",\"features\":" + exportData + "}"
+                    }
+                    break
+                default:
+            }
+            // as gpx generation is async we have to save it upon returned promise
+            // therefore skipping saving for gpx here
+            if (format !== "gpx") {
                 exportData = new Blob([exportData], {
-                  type: "application/xml;charset=utf-8"
-                });
-                FileSaver.saveAs(exportData, filename + extension);
-              },
-              function(error) {
-                orsSettingsFactory.requestSubject.onNext(false);
-                alert("There was an error generating the gpx file: " + error);
-              }
-            );
-            // geojsonData = L.polyline(geometry).toGeoJSON();
-            // geojsonData.properties.name = filename; // togpx is looking for name tag https://www.npmjs.com/package/togpx#gpx
-            // exportData = togpx(geojsonData, {
-            //   creator: "OpenRouteService.org"
-            // });
-            break;
-          case "kml":
-            geojsonData = L.polyline(geometry).toGeoJSON();
-            exportData = tokml(geojsonData);
-            break;
-          case "rawjson":
-            // removing nodes from the geometry data that is for sure not needed
-            // by 3'rd party...
-            delete geometry.extras;
-            delete geometry.geometryRaw;
-            delete geometry.$$hashKey;
-
-            // MARQ24: point_information have an massive effect on the actual file size!
-            // So I am not 100% sure if this should be included or not in the exported
-            // json data - personally I do not have any need for this info on my mobile
-            // client - that's why IMHO it can/should be removed from the raw output
-            delete geometry.point_information;
-
-            exportData = JSON.stringify(geometry);
-            extension = ".json";
-            break;
-          case "geojson":
-            if (geomType == "linestring") {
-              if (!options.elevation) {
-                angular.forEach(geometry, function(value) {
-                  value = value.splice(2, 1);
-                });
-              }
-              exportData = JSON.stringify(L.polyline(geometry).toGeoJSON());
-            } else if (geomType == "polygon") {
-              let isochrones = [];
-              for (let i = 0; i < geometry.length; i++) {
-                let properties = geometry[i].properties;
-                properties.id = i;
-                let c = geometry[i].geometry.coordinates;
-                for (let k = 0; k < c[0].length; k++) {
-                  let store = c[0][k][0];
-                  c[0][k][0] = c[0][k][1];
-                  c[0][k][1] = store;
-                }
-                const geojsonpolygon = {
-                  type: "Feature",
-                  properties: properties,
-                  geometry: {
-                    type: "Polygon",
-                    coordinates: c
-                  }
-                };
-                isochrones.push(geojsonpolygon);
-              }
-              exportData = JSON.stringify(isochrones);
-              exportData =
-                '{"type":"FeatureCollection","features":' + exportData + "}";
+                    type: "application/xml;charset=utf-8"
+                })
+                FileSaver.saveAs(exportData, filename + extension)
             }
-            break;
-          default:
         }
-        // as gpx generation is async we have to save it upon returned promise
-        // therefore skipping saving for gpx here
-        if (format !== "gpx") {
-          exportData = new Blob([exportData], {
-            type: "application/xml;charset=utf-8"
-          });
-          FileSaver.saveAs(exportData, filename + extension);
-        }
-      };
-      return orsExportFactory;
+        return orsExportFactory
     }
-  ])
-  /**
+])
+/**
  /*** Import Factory
- **/ .factory("orsImportFactory", [
-    "orsNamespaces",
-    orsNamespaces => {
-      var orsImportFactory = {};
-      /**
-       * Import a file and load to the map as a vector element
-       * @param {String} fileExt: file extension
-       * @param {String} fileContent: content of the file
-       * @return {Object} geometry: returns the geometry
-       */
-      orsImportFactory.importFile = (fileExt, fileContent) => {
-        let latlngs, features, geometry, i;
-        switch (fileExt) {
-          case "gpx":
-            features = omnivore.gpx.parse(fileContent);
-            break;
-          case "kml":
-            features = omnivore.kml.parse(fileContent);
-            break;
-          case "geojson":
-            features = L.geoJson(JSON.parse(fileContent));
-            break;
-          case "csv":
-            features = omnivore.csv.parse(fileContent);
-            // TO DO
-            break;
-          case "wkt":
-            features = omnivore.wkt.parse(fileContent);
-            // TO DO
-            break;
-          default:
-            alert("Error: file extension not is valid");
-        }
-        latlngs = features.getLayers()[0]._latlngs;
-        geometry = [];
-        // multi dimensional
-        if (latlngs[0].constructor === Array) {
-          for (i = 0; i < latlngs.length; i++) {
-            for (let j = 0; j < latlngs[i].length; j++) {
-              geometry.push([latlngs[i][j].lat, latlngs[i][j].lng]);
+ **/.factory("orsImportFactory", [
+    "orsNamespaces", orsNamespaces => {
+        var orsImportFactory = {}
+        /**
+         * Import a file and load to the map as a vector element
+         * @param {String} fileExt: file extension
+         * @param {String} fileContent: content of the file
+         * @return {Object} geometry: returns the geometry
+         */
+        orsImportFactory.importFile = (fileExt, fileContent) => {
+            let latlngs, features, geometry, i
+            switch (fileExt) {
+                case "gpx":
+                    features = omnivore.gpx.parse(fileContent)
+                    break
+                case "kml":
+                    features = omnivore.kml.parse(fileContent)
+                    break
+                case "geojson":
+                    features = L.geoJson(JSON.parse(fileContent))
+                    break
+                case "csv":
+                    features = omnivore.csv.parse(fileContent)
+                    // TO DO
+                    break
+                case "wkt":
+                    features = omnivore.wkt.parse(fileContent)
+                    // TO DO
+                    break
+                default:
+                    alert("Error: file extension not is valid")
             }
-          }
-          // one dimensional
-        } else {
-          for (i = 0; i < latlngs.length; i++) {
-            geometry.push([latlngs[i].lat, latlngs[i].lng]);
-          }
+            latlngs = features.getLayers()[0]._latlngs
+            geometry = []
+            // multi dimensional
+            if (latlngs[0].constructor === Array) {
+                for (i = 0; i < latlngs.length; i++) {
+                    for (let j = 0; j < latlngs[i].length; j++) {
+                        geometry.push([latlngs[i][j].lat, latlngs[i][j].lng])
+                    }
+                }
+                // one dimensional
+            } else {
+                for (i = 0; i < latlngs.length; i++) {
+                    geometry.push([latlngs[i].lat, latlngs[i].lng])
+                }
+            }
+            return geometry
         }
-        return geometry;
-      };
-      return orsImportFactory;
+        return orsImportFactory
     }
-  ]);
+])
